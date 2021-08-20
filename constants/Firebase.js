0f5207f02ea27260f135f951a2c1e0800a429f02
Nicolás Filippi Farmar <n.i.k.o-05@hotmail.com>
2021-08-19 23:04:42 -0300

    Added Firebase Auth urls & Auth login action

---
+++ constants/Firebase.js
@@ -0,0 +1,6 @@
+import { FIREBASE_API_KEY } from "./Keys";
+
+export const URL_AUTH = "https://identitytoolkit.googleapis.com/v1/accounts";
+export const URL_AUTH_SIGNUP = `${URL_AUTH}:signUp?key=${FIREBASE_API_KEY}`;
+export const URL_AUTH_LOGIN = `${URL_AUTH}:signInWithPassword?key=${FIREBASE_API_KEY}`;
+export const URL_AUTH_UPDATE = `${URL_AUTH}:update?key=${FIREBASE_API_KEY}`;
--- store/actions/auth.actions.js
+++ store/actions/auth.actions.js
@@ -1,7 +1,27 @@
+import { URL_AUTH_LOGIN } from "../../constants/Firebase";
+
 export const AUTH_LOGIN = "AUTH_LOGIN";
 
 export const loginWithEmail = (email, password) => {
   return async (dispatch) => {
-    dispatch({ type: AUTH_LOGIN, payload: { token: "", userId: "" } });
+    const res = fetch(URL_AUTH_LOGIN, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        email,
+        password,
+        returnSecureToken: true,
+      },
+    });
+
+    if (!res.ok) throw new Error("Usuario y/o Contraseña incorrectos");
+
+    const data = await res.json();
+    dispatch({
+      type: AUTH_LOGIN,
+      payload: { token: data.idToken, userId: data.localId },
+    });
   };
 };
